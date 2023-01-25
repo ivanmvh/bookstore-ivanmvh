f27d45816aa7cf683e943422e02a0236cc6bd621
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-25 00:53:59 -0500

    create book.js - Actions -Reducer
    Export Action Creators for your actions.
    Write your reducer and export it as default.
    - Define state changes for the actions that you created.
    - In case of unknown action - return the current state.

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,3 +3,25 @@ const REMOVE_BOOK = 'books/REMOVE_BOOK';
 
 const initialState = [];
 
+export const addBook = (book) => ({
+  type: ADD_BOOK,
+  book,
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  id,
+});
+
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.book];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export default booksReducer;
