6960749beb47ff9a6bd5584dab8fd35110b92fa0
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-25 05:57:11 -0500

    Import uuid

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,10 +1,15 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'books/ADD_BOOK';
 const REMOVE_BOOK = 'books/REMOVE_BOOK';
 
+const uniqueKey = uuidv4();
+
 const initialState = [
-  { key: 1, title: 'First Book', author: 'Author 1' },
-  { key: 2, title: 'Second Book', author: 'Author 2' },
-  { key: 3, title: 'Third Book', author: 'Author 3' },
+  { key: uniqueKey + 1, title: 'It works', author: 'first' },
+  { key: uniqueKey + 2, title: 'First Book', author: 'Author 1' },
+  { key: uniqueKey + 3, title: 'Second Book', author: 'Author 2' },
+  { key: uniqueKey + 4, title: 'Third Book', author: 'Author 3' },
 ];
 
 export const addBook = (book) => ({
@@ -22,7 +27,7 @@ const booksReducer = (state = initialState, action) => {
     case ADD_BOOK:
       return [...state, action.book];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload);
+      return state.filter((book) => book.key !== action.id);
     default:
       return state;
   }
