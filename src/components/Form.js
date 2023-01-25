17b8ab2f0abecc1e0e2dca8c70d3ccf5fcfc5acb
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-25 06:03:10 -0500

    Add onclick to assBtn - use setNewBook (useState)
    In your React component responsible for adding new books, add functionality for "Add new" button click:
    
    Import Redux useDispatch.
    Set the data inputs in the local React state (set title and author - NOTE: categories and comments are NOT part of this step).
    Takes the inputs from the state and generate a unique id and add them to an object.
    Once your new book object is ready to be submitted to Redux store, dispatch a corresponding action (import Action Creator that you created before and use it here).

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,50 @@
-const Form = () => (
-  <section className="formContainer">
-    <h2 className="formTitle">
-      ADD NEW BOOK
-    </h2>
-    <form className="form">
-      <input className="bookTitleForm" type="text" placeholder="Book title" />
-      <input className="bookAuthorForm" type="text" placeholder="Author" />
-      <button className="addBtn" type="submit">ADD BOOK</button>
-    </form>
-  </section>
-);
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+
+const Form = (props) => {
+  const { addNewBook } = props;
+
+  const uniqueKey = uuidv4();
+
+  const [newBook, setNewBook] = useState({
+    title: '',
+    author: '',
+  });
+
+  const handleChange = (e) => {
+    setNewBook({
+      ...newBook,
+      [e.target.name]: e.target.value,
+      key: uniqueKey,
+    });
+  };
+
+  const addBookToList = (e) => {
+    e.preventDefault();
+    addNewBook(newBook);
+    setNewBook({
+      title: '',
+      author: '',
+    });
+  };
+
+  return (
+    <section className="formContainer">
+      <h2 className="formTitle">
+        ADD NEW BOOK
+      </h2>
+      <form className="form">
+        <input className="bookTitleForm" type="text" placeholder="Book title" name="title" id="name" value={newBook.title} onChange={handleChange} required />
+        <input className="bookAuthorForm" type="text" placeholder="Author" name="author" id="author" value={newBook.author} onChange={handleChange} required />
+        <button className="addBtn" type="submit" onClick={addBookToList}>ADD BOOK</button>
+      </form>
+    </section>
+  );
+};
+
+Form.propTypes = {
+  addNewBook: PropTypes.func.isRequired,
+};
 
 export default Form;
