908b49eafd298a5c4a005bdd70a0fb31b0a628ce
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-25 05:33:16 -0500

    Make sure that list of books displays books from Redux store.
    useSelector, useDispatch } from 'react-redux'

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import BookCard from './BookCard';
+import Form from './Form';
+import { addBook, removeBook } from '../redux/books/books';
 
-class BookList extends React.PureComponent {
-  render() {
-    const bookArray = [
-      { key: 1, title: 'First Book', author: 'Author 1' },
-      // { key: 2, title: 'Second Book', author: 'Author 1' },
-      // { key: 3, title: 'Third Book', author: 'Author 1' },
-    ];
-    return (
+const BookList = () => {
+  const booksArray = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  const addNewBook = (book) => {
+    dispatch(addBook(book));
+  };
+
+  const deleteBook = (id) => {
+    dispatch(removeBook(id));
+  };
+
+  return (
+    <div className="bookListContainer">
       <ul className="booksList">
-        {bookArray.map((book) => (
+        {booksArray.map((book) => (
           <BookCard
+            removeBook={deleteBook}
             key={book.key}
+            id={book.key}
             title={book.title}
             author={book.author}
           />
         ))}
       </ul>
-    );
-  }
-}
+      <Form addNewBook={addNewBook} />
+    </div>
+  );
+};
 
 export default BookList;
