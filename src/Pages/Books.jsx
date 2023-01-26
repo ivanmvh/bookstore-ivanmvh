13d0f127081a6d9ad4f25af355fe57dbd6daed18
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-26 05:31:34 -0500

    Use addForm

--- src/Pages/Books.jsx
+++ src/Pages/Books.jsx
@@ -1,8 +1,10 @@
 import BookList from '../components/BookList';
+import AddForm from '../components/AddForm';
 
 const Books = () => (
   <section className="booksPage">
     <BookList />
+    <AddForm />
   </section>
 );
 
