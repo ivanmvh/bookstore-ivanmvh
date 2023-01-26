d7dc5ba5b5c115e37cd485e567817eda3a793fe2
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-26 06:47:53 -0500

    Add Action GET_BOOKS

--- src/Pages/Books.jsx
+++ src/Pages/Books.jsx
@@ -1,10 +1,8 @@
 import BookList from '../components/BookList';
-import AddForm from '../components/AddForm';
 
 const Books = () => (
   <section className="booksPage">
     <BookList />
-    <AddForm />
   </section>
 );
 
--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,48 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class BookCard extends React.PureComponent {
-  render() {
-    const { title, author } = this.props;
-    return (
-      <div className="book">
-        <div className="bookInfo">
-          <h2 className="bookTitle">{title}</h2>
-          <span className="bookAuthor">{author}</span>
-        </div>
-        <div className="bookActions">
-          <button type="button">Comments</button>
-          <button type="button">Remove</button>
-          <button type="button">Edit</button>
-        </div>
-        <div className="progressBlock">
-          <progress value="60" max="100"> 60%</progress>
-          <span>60%</span>
-        </div>
-        <div className="chapterInfo">
-          <div>
-            chapter info
-          </div>
-          <div>
-            introduction
-          </div>
-          <button type="button">
-            Update progress
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+const BookCard = (props) => {
+  const {
+    title, author, id, removeBook,
+  } = props;
 
-BookCard.defaultProps = {
-  title: 'Title',
-  author: 'Author',
+  const deleteBook = () => {
+    removeBook(id);
+  };
+
+  return (
+    <div className="book" id={id}>
+      <div className="bookInfo">
+        <h2 className="bookTitle">{title}</h2>
+        <span className="bookAuthor">{author}</span>
+      </div>
+      <div className="bookActions">
+        <button type="button">Comments</button>
+        <button type="button" onClick={deleteBook}>Remove</button>
+        <button type="button">Edit</button>
+      </div>
+      <div className="progressBlock">
+        <progress value="60" max="100"> 60%</progress>
+        <span>60%</span>
+      </div>
+      <div className="chapterInfo">
+        <div>
+          chapter info
+        </div>
+        <div>
+          introduction
+        </div>
+        <button type="button">
+          Update progress
+        </button>
+      </div>
+    </div>
+  );
 };
 
 BookCard.propTypes = {
-  title: PropTypes.string,
-  author: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
 
 export default BookCard;
