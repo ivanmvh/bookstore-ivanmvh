dab2eecb843f18d15b4cda9a637f81872d7272a5
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-26 05:30:42 -0500

    Create removeBook  with Thunk

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,12 +2,14 @@ import { v4 as uuidv4 } from 'uuid';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import API_BOOKS_URL from './api-books-url';
 
-const ADD_BOOK = 'bookstore-ivanmvh/books/ADD_BOOK ';
-const REMOVE_BOOK = 'bookstore-ivanmvh/books/REMOVE_BOOK';
+const ADD_BOOK = 'books/ADD_BOOK';
+const REMOVE_BOOK = 'books/REMOVE_BOOK';
 
 const uniqueKey = uuidv4();
 
-const initialState = [];
+const initialState = [
+  { key: uniqueKey, title: 'It works', author: 'first' },
+];
 
 export const addBook = (book) => ({
   type: ADD_BOOK,
@@ -15,19 +17,29 @@ export const addBook = (book) => ({
 });
 
 export const addBookAsync = createAsyncThunk(ADD_BOOK, async (book) => {
-  const response= await fetch(`${API_BOOKS_URL}`, {
+  const result = await fetch(`${API_BOOKS_URL}`, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(book)
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(book),
   }).then((response) => response.text())
     .then((result) => (result === 'Created' ? book : null));
+  return result;
 });
-
 
 export const removeBook = (id) => ({
   type: REMOVE_BOOK,
   id,
+});
+
+export const removeBookAsync = createAsyncThunk(REMOVE_BOOK, async (id) => {
+  const result = await fetch(`${API_BOOKS_URL}${id}`, {
+    method: 'DELETE',
+    headers: {
+      'content-Type': 'application/json',
+    },
+  }).then((response) => response.text())
+    .then((result) => (result === 'The book was deleted successfully!' ? id : null));
+  return result;
 });
 
 const booksReducer = (state = initialState, action) => {
