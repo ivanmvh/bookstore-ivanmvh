6a62f4fe4c3a3cbb24c614817ef4cb93d9b01cf6
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-26 03:10:07 -0500

    Use fetch with createAsyncThunk for making API calls.

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,11 @@
+import { v4 as uuidv4 } from 'uuid';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import API_BOOKS_URL from './api-books-url';
+
 const ADD_BOOK = 'bookstore-ivanmvh/books/ADD_BOOK ';
 const REMOVE_BOOK = 'bookstore-ivanmvh/books/REMOVE_BOOK';
+
+const uniqueKey = uuidv4();
 
 const initialState = [];
 
@@ -7,6 +13,17 @@ export const addBook = (book) => ({
   type: ADD_BOOK,
   book,
 });
+
+export const addBookAsync = createAsyncThunk(ADD_BOOK, async (book) => {
+  const response= await fetch(`${API_BOOKS_URL}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(book)
+  }).then((response) => response.text())
+    .then((result) => (result === 'Created' ? book : null));
+});
+
 
 export const removeBook = (id) => ({
   type: REMOVE_BOOK,
@@ -18,7 +35,7 @@ const booksReducer = (state = initialState, action) => {
     case ADD_BOOK:
       return [...state, action.book];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload);
+      return state.filter((book) => book.key !== action.id);
     default:
       return state;
   }
