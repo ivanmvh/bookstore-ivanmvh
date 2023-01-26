1ef2271e03cfcf96e0f6c914258d3cdb7d18ff05
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-26 02:51:47 -0500

    Update api-books-url.js

--- src/redux/books/api-books-url.js
+++ src/redux/books/api-books-url.js
@@ -1,2 +1,4 @@
 const API_BOOKS_ID = 'b4G6ssxfJvvQNF9n0zoX';
-const API_BOOKS_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${API_ID}`;
+const API_BOOKS_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${API_ID}/books/`;
+
+export default API_BOOKS_URL;
