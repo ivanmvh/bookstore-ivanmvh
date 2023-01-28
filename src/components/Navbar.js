a2ac471258f743cd69abe17a18a5a7cf9987358b
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-28 12:00:22 -0500

    Trying to align userBtn to the right

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,12 @@ import { FaUserGraduate } from 'react-icons/fa';
 
 const Navbar = () => (
   <div className="navBar">
+    <p className="logo">Bookstore CMS</p>
     <ul className="nav">
-      <p className="logo">Bookstore CMS</p>
       <li className="navLink"><NavLink to="/" end className={({ isActive }) => (isActive ? 'currentPage' : undefined)}>BOOKS</NavLink></li>
       <li className="navLink"><NavLink to="/categories" end className={({ isActive }) => (isActive ? 'currentPage' : undefined)}>CATEGORIES</NavLink></li>
-      <div className="userBtn"><FaUserGraduate style={{ color: '#0290ff' }} /></div>
     </ul>
+    <div className="userBtn"><FaUserGraduate style={{ color: '#0290ff' }} /></div>
   </div>
 );
 
